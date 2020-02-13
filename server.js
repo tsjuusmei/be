2d8ffe398e28c1f94d8b58870869f42b24f70b4c
tsjuusmei <guusmay2@hotmail.com>
2020-02-13 19:44:11 +0100

    week1 af

--- server.js
+++ server.js
@@ -5,24 +5,10 @@ const port = 3000
 
 app.use('/static', express.static('public'))
 app.listen(port, () => console.log('listening on port ' + port))
-app.get('/', (req, res) => {
-    const route = req.url
-    console.log(route)
-
-    if (route === '/'){
-        res.send('hello world')
-    } else if (route === '/about') {
-        res.send('about')
-    } else if (route === '/contact') {
-        res.send('contact')
-    } else {
-        res.statusCode = 404
-        res.send('not found\n')
-    }
-})
-
-
-// app.get('/about', (req, res) => res.send('about page'))
+app.get('/', (req, res) => res.send('index'))
+app.get('/about', (req, res) => res.send('about page'))
+app.get('/contact', (req, res) => res.send('contact page'))
+app.use((req, res) => res.status(404).send('404'))
 
 console.log(cc('back-end'))
 
