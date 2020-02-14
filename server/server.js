14e01342b804485a85258d8008d9f663b1c1421b
tsjuusmei <guusmay2@hotmail.com>
2020-02-14 14:47:35 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -4,7 +4,13 @@ const app = express()
 const port = 3000
 
 app.use('/static', express.static('public'))
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log('listening on port ' + port))
+app.get('/', (req, res) => res.send('index'))
+app.get('/about', (req, res) => res.send('about page'))
+app.get('/contact', (req, res) => res.send('contact page'))
+app.use((req, res) => res.status(404).send('404'))
 
 console.log(cc('back-end'))
+
+
+// push command: git push -f origin master
