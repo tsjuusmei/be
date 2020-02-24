b2328e5f397e4c0767eb52591966a1e2a0d602c1
tsjuusmei <guusmay2@hotmail.com>
2020-02-24 13:37:00 +0100

    week 2 les 1, shit werkt wauw

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const cc = require('camelcase')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('static'))
+
 app.listen(port, () => console.log('listening on port ' + port))
-app.get('/', (req, res) => res.send('index'))
-app.get('/about', (req, res) => res.send('about page'))
-app.get('/contact', (req, res) => res.send('contact page'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/static/index.html')))
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname + '/static/about.html')))
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/static/contact.html')))
+
 app.use((req, res) => res.status(404).send('404'))
 
 console.log(cc('back-end'))
-
 
 // push command: git push -f origin master
